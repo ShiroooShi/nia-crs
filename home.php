559acf3660a536320d8ba57d8852a1d40886c62f
ShiroooShi <adrianshiro2@gmail.com>
2024-10-21 11:35:25 +0800

    Updated

--- home.php
+++ home.php
@@ -151,8 +151,6 @@ include 'fetch_data.php';
             }
         });
     </script>
-
-
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
--- records.php
+++ records.php
@@ -295,9 +295,6 @@ function escape($string)
         </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script>
         // Show message function
         function showMessage(text, type) {
@@ -444,6 +441,9 @@ function escape($string)
             input.value = formattedValue.trim();
         }
     </script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 </body>
 
